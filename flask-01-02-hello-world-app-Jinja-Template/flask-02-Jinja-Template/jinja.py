31ec7f76ad01f348a4115d3c912d1e2e225b3f12
UygurSoytemiz <ussu78@gmail.com>
2021-06-20 23:50:48 -0400

    our applications

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja-Template/jinja.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1=12, number2=15)
+
+@app.route('/sum')
+def sum():
+    num1, num2 =27,15
+    return render_template('body.html', value1=num1, 
+    value2=num2, value3=num1+num2 )
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
